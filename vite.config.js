9b6e37a7184cf63d7cf14f96eb4d87d0ceaaba87
Tiz Code <opawaletimi@gmail.com>
2026-01-22 06:47:28 +0100

    Remove build configuration from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
+  base: '/',
 })
-```
-
-### **5. .npmrc** - Should be:
-```
-legacy-peer-deps=true
